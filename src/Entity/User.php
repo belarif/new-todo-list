49377dac8683bdcf05541b48de8720cfd14739fd
Belarif <b.ocine@live.fr>
2022-06-27 12:10:59 +0200

    TDL2: upgrading tests of User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -80,7 +80,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @see PasswordAuthenticatedUserInterface
      */
-    public function getPassword(): string
+    public function getPassword(): ?string
     {
         return $this->password;
     }
