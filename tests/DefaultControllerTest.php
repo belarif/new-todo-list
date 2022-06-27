9b24391f80c720bb33e240080fc8bef5dfa8534e
Belarif <b.ocine@live.fr>
2022-06-27 12:42:38 +0200

    TDL3: upgrading tests of methods DefaultController

---
+++ tests/DefaultControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class DefaultControllerTest extends WebTestCase
+{
+    public function test_it_should_display_homepage(): void
+    {
+        $client = self::createClient();
+
+        $urlGenerator = $client->getContainer()->get('router');
+
+        $crawler = $client->request('GET', $urlGenerator->generate('homepage'));
+
+        $response = $client->getResponse();
+
+        self::assertTrue($response->isOk());
+        self::assertSame(
+            "Bienvenue sur Todo List, l'application vous permettant de gérer l'ensemble de vos tâches sans effort !",
+            $crawler->filter('h1')->first()->text()
+        );
+    }
+}
