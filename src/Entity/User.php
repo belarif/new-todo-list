8d6c5f079635af19677fd550596b796c70c1f5d4
Belarif <b.ocine@live.fr>
2022-08-02 12:45:19 +0200

    TDL14: creating functional test that test redirection when trying to create an existing user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -87,7 +87,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function setRoles(array $roles): self
     {
-        $this->roles = $roles;
+        $this->roles = new ArrayCollection($roles);
 
         return $this;
     }
