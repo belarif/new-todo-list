95d1d57048f9ac1d42bb5ad6ed7ec2180dca0764
Belarif <b.ocine@live.fr>
2022-08-05 12:24:35 +0200

    TDL18: ignoring logout() function from tests

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use PHPUnit\Framework\TestCase;
 
 class SecurityController extends AbstractController
 {
@@ -24,6 +25,8 @@ class SecurityController extends AbstractController
 
     /**
      * @throws Exception
+     *
+     * @codeCoverageIgnore
      */
     #[Route('/logout', name: 'app_logout', methods: ['GET'])]
     public function logout()
