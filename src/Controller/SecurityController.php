af0cad80acc083a60a6f6f47843b944f4b5e7a89
Belarif <b.ocine@live.fr>
2022-08-11 14:16:25 +0200

    TDL23: fix src repository with PHP Cs fixer

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,7 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use PHPUnit\Framework\TestCase;
 
 class SecurityController extends AbstractController
 {
